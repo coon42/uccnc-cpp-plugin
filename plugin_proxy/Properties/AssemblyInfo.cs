3a6176ac5791956d1cb8e97f93e3ddb54bfe4c2f
coon <coon@c-base.org>
2017-12-16 21:35:59 +0100

    plugin_gateway -> plugin_proxy #2

--- plugin_proxy/Properties/AssemblyInfo.cs
+++ plugin_proxy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("plugin_gateway")]
+[assembly: AssemblyTitle("plugin_proxy")]
 [assembly: AssemblyDescription("C++ binding plugin for UCCNC")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("plugin_gateway")]
+[assembly: AssemblyProduct("plugin_proxy")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
